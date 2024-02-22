473fd1cb6f0c26de796ae71f2547806c38298506
Mulindwa Mark Myles <mylesmark752@gmail.com>
2024-02-22 18:53:48 +0300

    04

--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+import requests, datetime
 
 
 # Create your views here.
 def home(request):
-	return HttpResponse("Hii!")
+	API_KEY  = open("../API_KEY", 'r').read()
+	current_weather_url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
+	forecast_url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid={}"
